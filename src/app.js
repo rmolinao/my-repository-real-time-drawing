216f5e64f2bf320f2b0670b8fb81f5d8e72fba86
Rafael Molina Osias <engineer.raflash@gmail.com>
2024-03-12 07:00:57 -0500

    configraciones  para subir mi proyecto a heroku

--- src/app.js
+++ src/app.js
@@ -2,14 +2,16 @@ const express = require('express');
 const http = require('http');
 const path = require('path');
 const socketio = require('socket.io');
+const cors = require('cors');
 
 const app = express()
 const server = http.createServer(app);
 const io = socketio(server);
 require('./socket')(io);
 
-app.set('port',3001);
+app.set('port', process.env.PORT||3001);
 app.use(express.static(path.join(__dirname,'public')));
+app.use(cors());
 
 server.listen(app.get('port'), () => {
     console.log('Aplicacion corriendo en el puerto ' + app.get('port'));
